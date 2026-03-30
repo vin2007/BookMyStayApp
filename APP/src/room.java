f93c45c6ca2a253d74ab9f4ac43a8bdc0a712c98
vin2007 <vineshreddy2006@gmail.com>
2026-03-30 14:54:15 +0530

    UC10

--- APP/src/room.java
+++ APP/src/room.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+// Custom Exception Class
 class InvalidBookingException extends Exception {
 
     public InvalidBookingException(String message) {
@@ -10,7 +11,7 @@ class InvalidBookingException extends Exception {
 }
 
 
-
+// Room Inventory Manager
 class RoomInventory {
 
     private Map<String, Integer> inventory;
@@ -77,6 +78,7 @@ class RoomInventory {
 }
 
 
+// Booking Validator Service
 class BookingValidatorService {
 
     public void validateBookingInput(String guestName,
@@ -100,7 +102,7 @@ class BookingValidatorService {
 }
 
 
-
+// Main Class
 public class UseCase9ErrorHandlingValidation {
 
     public static void main(String[] args) {
